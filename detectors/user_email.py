a1526a489554e871359edd3e57e41cb1f650d9da
Moritz Lipp <mlq@pwmt.org>
2013-07-25 20:33:54 +0200

    Add user templates

--- detectors/user_email.py
+++ detectors/user_email.py
@@ -2,14 +2,10 @@ from roundup import i18n
 from validate_email import validate_email
 
 def audit_email(db, cl, nodeid, newvalues):
-  if user.has_key('address'):
-    if not validate_email(user['address']):
-      raise ValueError(i18n.gettext("Email address %s is invalid", user['address']))
-
-  user_main = db.user.stringFind(address=address)
-  user_other = [u for u in db.user.filter(None, {'alternate_addresses' : address}) if u != nodeid]
-  if user_main or user_other:
-    raise ValueError(i18n.gettext("Email address %s is already in use", address))
+  if newvalues.has_key('address'):
+    address = newvalues['address']
+    if not validate_email(address):
+      raise ValueError(i18n.gettext("Email address %s is invalid", address))
 
 def init(db):
   db.user.audit('set',    audit_email)
--- detectors/user_role.py
+++ detectors/user_role.py
@@ -1,11 +1,12 @@
 from roundup import i18n
 
 def audit_roles(db, cl, nodeid, newvalues):
-  new_roles = newvalues.get('roles')
-  if new_roles:
-    for role in [r.lower().strip() for r in newroles.split(',')]:
-      if role and not db.security.role.has_key(role):
-        raise ValueError(i18n.gettext("Role %s does not exist", role))
+  if newvalues.has_key('roles'):
+    new_roles = newvalues.get('roles')
+    if new_roles:
+      for role in [r.lower().strip() for r in new_roles.split(',')]:
+        if role and not db.security.role.has_key(role):
+          raise ValueError(i18n.gettext("Role %s does not exist", role))
 
 def init(db):
   db.user.audit('set',    audit_roles)
