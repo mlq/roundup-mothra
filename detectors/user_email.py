1fba82c78e9bef78717bb679b4ab804729233f36
Moritz Lipp <mlq@pwmt.org>
2013-07-26 12:15:14 +0200

    Fix issue_type check

--- detectors/user_email.py
+++ detectors/user_email.py
@@ -5,7 +5,7 @@ def audit_email(db, cl, nodeid, newvalues):
   if newvalues.has_key('address'):
     address = newvalues['address']
     if not validate_email(address):
-      raise ValueError(i18n.gettext("Email address %s is invalid", address))
+      raise ValueError(i18n.gettext("Email address %s is invalid"), address)
 
 def init(db):
   db.user.audit('set',    audit_email)
--- detectors/user_role.py
+++ detectors/user_role.py
@@ -6,7 +6,7 @@ def audit_roles(db, cl, nodeid, newvalues):
     if new_roles:
       for role in [r.lower().strip() for r in new_roles.split(',')]:
         if role and not db.security.role.has_key(role):
-          raise ValueError(i18n.gettext("Role %s does not exist", role))
+          raise ValueError(i18n.gettext("Role %s does not exist"), role)
 
 def init(db):
   db.user.audit('set',    audit_roles)
